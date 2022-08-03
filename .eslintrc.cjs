b40e5bf04645c66544aa24521c690e9b057d44a4
Viktor Romanyuk <v.romanyuk@softonix.org>
2022-08-04 00:53:27 +0300

    eslint updates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -53,7 +53,8 @@ module.exports = {
     '@typescript-eslint/indent': ['error', 2, { VariableDeclarator: 4 }],
     '@typescript-eslint/type-annotation-spacing': ['error', {
       before: false,
-      after: true
+      after: true,
+      overrides: { arrow: { before: true, after: true } }
     }],
 
     '@typescript-eslint/member-delimiter-style': ['error',
