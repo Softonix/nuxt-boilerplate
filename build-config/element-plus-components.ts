a3d5264574fff27015235c1a391b3a1bd5fc481e
Viktor Romanyuk <v.romanyuk@softonix.org>
2022-08-15 22:28:33 +0300

    updated nuxt to rc8

--- build-config/element-plus-components.ts
+++ build-config/element-plus-components.ts
@@ -1,9 +1,9 @@
 import { Component, ComponentsDir, ScanDir } from '@nuxt/schema'
 import { paramCase } from 'change-case'
 
-import * as Element from 'element-plus/dist/index.full.min'
+import * as Element from 'element-plus/dist/index.full.min.mjs'
 
-export * from 'element-plus/dist/index.full.min'
+export * from 'element-plus/dist/index.full.min.mjs'
 
 export function buildElementComponents (components: (Component | ComponentsDir | ScanDir)[]) {
   Object.keys(Element).forEach((component) => {
