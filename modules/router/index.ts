9a71f887f98580d2d642bb3f199900e1e515a227
Viktor Romanyuk <v.romanyuk@softonix.org>
2022-10-08 12:25:06 +0300

    updated router module

--- modules/router/index.ts
+++ modules/router/index.ts
@@ -1,8 +1,16 @@
 import { buildRouter } from './runtime/router-builder'
-import { defineNuxtModule } from '@nuxt/kit'
+import {
+  defineNuxtModule,
+  addPlugin,
+  createResolver
+} from '@nuxt/kit'
 
 export default defineNuxtModule({
   hooks: {
     'pages:extend': buildRouter
+  },
+  setup () {
+    const { resolve } = createResolver(import.meta.url)
+    addPlugin(resolve('./runtime/route-names.plugin'))
   }
 })
